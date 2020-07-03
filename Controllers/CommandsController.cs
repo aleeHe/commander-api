49f0ecd09f685f6848061c065f4b2e77934e7d6b
Ali Heshmati <a.heshmati96@gmail.com>
2020-07-03 19:41:24 +0430

    modify CommandsController

--- Controllers/CommandsController.cs
+++ Controllers/CommandsController.cs
@@ -9,7 +9,12 @@ namespace Commander.Controllers
     [ApiController]
     public class CommandsController : ControllerBase
     {
-        private MockCommanderRepo _repository = new MockCommanderRepo();
+        private readonly ICommanderRepo _repository;
+
+        public CommandsController(ICommanderRepo repository)
+        {
+            _repository = repository;
+        }
 
         [HttpGet]
         public ActionResult<IEnumerable<Command>> GetAllCommands()
