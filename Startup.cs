64bf43cd2bebd93185b3984784bda4e34740079f
Ali Heshmati <a.heshmati96@gmail.com>
2020-07-04 23:41:10 +0430

    modify startup

--- Startup.cs
+++ Startup.cs
@@ -31,7 +31,7 @@ namespace Commander
 
             services.AddControllers();
 
-            services.AddScoped<ICommanderRepo, MockCommanderRepo>();
+            services.AddScoped<ICommanderRepo, SqlCommanderRepo>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
