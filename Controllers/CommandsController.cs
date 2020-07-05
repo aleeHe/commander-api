68c7a3de41cfb7aeb9f942331d977ba8e01505a7
Ali Heshmati <a.heshmati96@gmail.com>
2020-07-05 14:59:10 +0430

    add DeleteCommand route

--- Controllers/CommandsController.cs
+++ Controllers/CommandsController.cs
@@ -94,5 +94,20 @@ namespace Commander.Controllers
 
             return NoContent();
         }
+
+        [HttpDelete("{id}")]
+        public ActionResult DeleteCommand(int id)
+        {
+            var commandModelFromRepo = _repository.GetCommandById(id);
+
+            if (commandModelFromRepo == null)
+            {
+                return NotFound();
+            }
+            _repository.DeleteCommand(commandModelFromRepo);
+            _repository.SaveChanges();
+
+            return NoContent();
+        }
     }
 }
