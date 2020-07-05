d94effcc1b446d32412cee38760c24a208f676dd
Ali Heshmati <a.heshmati96@gmail.com>
2020-07-05 14:44:21 +0430

    add Newtonsoft to Startup

--- Startup.cs
+++ Startup.cs
@@ -13,6 +13,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json.Serialization;
 
 namespace Commander
 {
@@ -30,7 +31,10 @@ namespace Commander
         {
             services.AddDbContext<CommanderContext>(opt => opt.UseNpgsql(Environment.GetEnvironmentVariable("CommanderDbConnectionString")));
 
-            services.AddControllers();
+            services.AddControllers().AddNewtonsoftJson(s =>
+            {
+                s.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+            });
 
             services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
